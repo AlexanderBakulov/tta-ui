169cd3f326ab23837a80a469bed0c1ec8c5286d8
Alexander Bakulov <asbakulov@gmail.com>
2020-10-11 11:20:21 +0600

    first commit

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+
+import './app.css';
+
+const App = () => {
+  return (
+    <div>Hello</div>
+    
+  );
+};
+
+export default App;
